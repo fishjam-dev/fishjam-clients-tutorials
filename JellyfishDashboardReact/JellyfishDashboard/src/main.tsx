64e1fd803c5166b8fd5b8f579225ad4d7268eb05
Crackhoff <michalzkato@gmail.com>
2023-08-31 10:35:49 +0200

    Minor fixes

--- JellyfishDashboardReact/JellyfishDashboard/src/main.tsx
+++ JellyfishDashboardReact/JellyfishDashboard/src/main.tsx
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom/client";
 import { App, JellyfishContextProvider } from "./components/App";
 import "./styles.css";
 
-// Create a Membrane client instance
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <JellyfishContextProvider>
