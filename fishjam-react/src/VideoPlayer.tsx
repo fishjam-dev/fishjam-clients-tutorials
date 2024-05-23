bdd6830b332e804efa5db83edea6adfbb743e765
Kamil Stasiak <kamilstasiaknew@gmail.com>
2024-05-23 16:12:26 +0200

    Update react tutorial

---
+++ fishjam-react/src/VideoPlayer.tsx
@@ -0,0 +1,21 @@
+import { RefObject, useEffect, useRef } from "react";
+type Props = {
+    stream: MediaStream | null | undefined;
+};
+
+const VideoPlayer = ({ stream }: Props) => {
+    const videoRef: RefObject<HTMLVideoElement> = useRef<HTMLVideoElement>(null);
+
+    useEffect(() => {
+        if (!videoRef.current) return;
+        videoRef.current.srcObject = stream || null;
+    }, [stream]);
+
+    return (
+        <div className="video-container">
+            <video autoPlay playsInline muted ref={videoRef} />
+        </div>
+    );
+};
+
+export default VideoPlayer;
